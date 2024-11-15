1d1c452a2c8294e46180da9c187989a53e59ad9a
Apo-Eclipse <160615838+Apo-Eclipse@users.noreply.github.com>
2024-11-15 19:48:38 +0200

    Delete bd-a1 directory

--- bd-a1/final.sh
+++
@@ -1,7 +0,0 @@
-docker cp <container_id>:/home/doc-bd-a1/res_dpre.csv ./bd-a1/service-result/
-docker cp <container_id>:/home/doc-bd-a1/eda-in-1.txt ./bd-a1/service-result/
-docker cp <container_id>:/home/doc-bd-a1/eda-in-2.txt ./bd-a1/service-result/
-docker cp <container_id>:/home/doc-bd-a1/eda-in-3.txt ./bd-a1/service-result/
-docker cp <container_id>:/home/doc-bd-a1/vis.png ./bd-a1/service-result/
-docker cp <container_id>:/home/doc-bd-a1/k.txt ./bd-a1/service-result/
-docker stop <container_id>
--- bd-a1/vis.py
+++
@@ -1,9 +0,0 @@
-import matplotlib.pyplot as plt
-import seaborn as sns
-
-def visualize(df):
-    sns.histplot(df['price'], bins=7, kde=False)
-    plt.xlabel('Price')
-    plt.ylabel('Frequency')
-    plt.title('Histogram of Prices')
-    plt.savefig('vis.png')
